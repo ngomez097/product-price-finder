473a2f53e085f7adeb257db0bbd4753394ad08fe
ngomez097 <nicoantogd@gmail.com>
2023-06-12 15:02:00 -0300

    feat: initial project configuration

---
+++ src/main/java/com/ngomez/price_finder/ProductPriceFinderApplication.java
@@ -0,0 +1,13 @@
+package com.ngomez.price_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPriceFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPriceFinderApplication.class, args);
+	}
+
+}
